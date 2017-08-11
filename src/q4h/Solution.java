eaa561905bc9c06f97b40f9761b636159be9a81a
liufanwen <1019851086@qq.com>
2017-08-11 14:10:41 +0800

    中值排序

--- src/q4h/Solution.java
+++ src/q4h/Solution.java
@@ -19,17 +19,52 @@ public class Solution {
 	 * @return
 	 */
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		int j = 0;
-		int[] result =new int[nums1.length+nums2.length];
-		for (int i = 0; i < nums1.length; ) {
-			//in
+		if (nums1.length == 0 && nums2.length == 0) {
+			return 0.0;
 		}
-		return 0;
+		int j = 0;
+		int i = 0;
+		int temp = 0;
+		int[] result = new int[nums1.length + nums2.length];
+		for (int x = 0; x < result.length; x++) {
+			if (i < nums1.length && j < nums2.length) {
+				if (nums1[i] < nums2[j]) {
+					temp = nums1[i];
+					i++;
+				} else {
+					temp = nums2[j];
+					j++;
+				}
+			} else {
+				if (i < nums1.length) {
+					temp = nums1[i];
+					i++;
+				}
+				if (j < nums2.length) {
+					temp = nums2[j];
+					j++;
+				}
+			}
+
+			result[x] = temp;
+		}
+		int num = result.length;
+		double result1 = 0.0;
+		if (result.length < 2) {
+			result1 = result[0];
+		} else {
+			if (num % 2 == 0) {
+				result1 = Double.valueOf(result[num / 2 - 1] + result[num / 2]) / 2;
+			} else {
+				result1 = result[(num-1) / 2 ];
+			}
+		}
+		return result1;
 	}
 
 	public static void main(String[] args) {
-		int[] nums1 = { 1, 2 };
-		int[] nums2 = { 3, 4 };
+		int[] nums1 = { 1, 3 };
+		int[] nums2 = { 2 };
 		Solution s = new Solution();
 		System.out.println(s.findMedianSortedArrays(nums1, nums2));
 	}
