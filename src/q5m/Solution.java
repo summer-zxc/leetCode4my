a574156594bc7bda634617dc2c207527a8d88479
liufanwen <1019851086@qq.com>
2017-08-17 17:55:49 +0800

    最长回文数

---
+++ src/q5m/Solution.java
@@ -0,0 +1,26 @@
+package q5m;
+
+/**
+ * Given a string s, find the longest palindromic substring in s. You may assume
+ * that the maximum length of s is 1000.
+ * 
+ * Input: "babad"
+ * 
+ * Output: "bab"
+ * 
+ * Note: "aba" is also a valid answer
+ * 
+ * @author liufanwen
+ * @2017年8月17日
+ */
+public class Solution {
+	public String longestPalindrome(String s) {
+		return s;
+
+	}
+
+	public static void main(String[] args) {
+		Solution su = new Solution();
+
+	}
+}
